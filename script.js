9a1bbac7ae26a14e3eb109a96cc8f0c64f135f79
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-12 23:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
-}
+function convertToBinary() {
+    const decimalInput = document.getElementById('decimalInput').value;
+    const binaryOutput = document.getElementById('binaryOutput');
 
-window.decimalToBinary = decimalToBinary;
+    // Check if the input is a valid number
+    if (isNaN(decimalInput)) {
+        binaryOutput.textContent = 'Invalid input. Please enter a valid number.';
+        return;
+    }
+
+    // Function to convert decimal to binary
+    function decimalToBinary(decimal) {
+        return (decimal >>> 0).toString(2);
+    }
+
+    // Get the binary equivalent
+    const binaryValue = decimalToBinary(parseInt(decimalInput));
+
+    binaryOutput.textContent = `Binary Equivalent: ${binaryValue}`;
+}
